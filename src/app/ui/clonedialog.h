8ab733909821ac3721a3d4704abb36bc3cec94cf
NPlebanskii <nazar796@gmail.com>
2015-06-29 23:20:08 +0300

    Documentation of class CloneDialog.

--- src/app/ui/clonedialog.h
+++ src/app/ui/clonedialog.h
@@ -19,6 +19,11 @@ public:
     ~CloneDialog();
 
 private slots:
+    /*!
+     * \brief Opens file dialog, in which user selects proper directory.
+     * After selecting, full path of selected directory is shown in lineEdit "Clone to:".
+     * If user pressed "Cancel" button, nothing is changed in lineEdit.
+     */
     void browse();
 
 private:
