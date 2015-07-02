42e0d65d94980e1f7284790e975a0df57600dc2d
molochiy <molochiy@gmail.com>
2015-07-03 02:42:44 +0300

    Added label for error's message and button ok, which close open dialog

--- src/app/ui/errordialog.cpp
+++ src/app/ui/errordialog.cpp
@@ -5,10 +5,17 @@ ErrorDialog::ErrorDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ErrorDialog)
 {
-    ui->setupUi(this);
+    ui->setupUi(this);    
+    this->setFixedSize(400,300);
 }
 
 ErrorDialog::~ErrorDialog()
 {
     delete ui;
+    connect(pushButtonOk,SIGNAL(clicked(bool)),this,SLOT(on_pushButtonOk_clicked()));
+}
+
+void ErrorDialog::on_pushButtonOk_clicked()
+{
+    this->close();
 }
--- src/app/ui/errordialog.h
+++ src/app/ui/errordialog.h
@@ -15,6 +15,9 @@ public:
     explicit ErrorDialog(QWidget *parent = 0);
     ~ErrorDialog();
 
+private slots:
+    void on_pushButtonOk_clicked();
+
 private:
     Ui::ErrorDialog *ui;
 };
