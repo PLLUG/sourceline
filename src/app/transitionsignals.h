ad1c8787c41a5631385e249a9f8fe49aef576e22
NPlebanskii <nazar796@gmail.com>
2015-07-14 17:44:14 +0300

    Added header and null pointer initialization in class TransitionSignals.

--- src/app/transitionsignals.h
+++ src/app/transitionsignals.h
@@ -1,6 +1,29 @@
 #ifndef TRANSITIONSIGNALS
 #define TRANSITIONSIGNALS
 
+/*******************************************************************************
+***                                                                          ***
+***    SourceLine - Crossplatform VCS Client.                                ***
+***    Copyright (C) 2015  by                                                ***
+***            Nazarii Plebanskii (nazar796@gmail.com)                       ***
+***                                                                          ***
+***    This file is part of SourceLine Project.                              ***
+***                                                                          ***
+***    SourceLine is free software: you can redistribute it and/or modify    ***
+***    it under the terms of the GNU General Public License as published by  ***
+***    the Free Software Foundation, either version 3 of the License, or     ***
+***    (at your option) any later version.                                   ***
+***                                                                          ***
+***    SourceLine is distributed in the hope that it will be useful,         ***
+***    but WITHOUT ANY WARRANTY; without even the implied warranty of        ***
+***    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ***
+***    GNU General Public License for more details.                          ***
+***                                                                          ***
+***    You should have received a copy of the GNU General Public License     ***
+***    along with this program.  If not, see <http://www.gnu.org/licenses/>. ***
+***                                                                          ***
+*******************************************************************************/
+
 #include <QObject>
 
 /*!
@@ -13,7 +36,7 @@ class TransitionSignals : public QObject
 {
     Q_OBJECT
 public:
-    explicit TransitionSignals(QObject *parent) : QObject(parent)
+    explicit TransitionSignals(QObject *parent = 0) : QObject(parent)
     {
     }
 
