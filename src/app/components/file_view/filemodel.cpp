ea5691617f269fd222af28f38b9a366ffd790205
kopylets2580 <kopylets2580@gmail.com>
2015-08-03 15:32:09 +0300

    add roles

---
+++ src/app/components/file_view/filemodel.cpp
@@ -0,0 +1,7 @@
+#include "filemodel.h"
+
+FileModel::FileModel()
+{
+
+}
+
---
+++ src/app/components/file_view/filemodel.h
@@ -0,0 +1,12 @@
+#ifndef FILEMODEL_H
+#define FILEMODEL_H
+
+#include <QWidget>
+
+class FileModel : public QSystemFileModel
+{
+public:
+    FileModel();
+};
+
+#endif // FILEMODEL_H
