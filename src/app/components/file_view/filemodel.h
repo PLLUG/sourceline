1ece75a426d852589df253fbc71095472a48517a
kopylets2580 <kopylets2580@gmail.com>
2015-08-03 16:39:33 +0300

    add comments

--- src/app/components/file_view/filemodel.h
+++ src/app/components/file_view/filemodel.h
@@ -8,18 +8,39 @@
 class FileModel : public QFileSystemModel
 {
 public:
+    /*!
+     * \brief roles for icons for files
+     */
     enum Roles {
         FileVisibleRole = Qt::DecorationRole,
         FileModifiedRole = Qt::UserRole + 1,
     };
 
+    /*!
+     * \brief constructor FileModel
+     * \param parent
+     */
     FileModel(QWidget *parent);
 
+    /*!
+     * \brief set icon for role
+     * \param index item on model
+     * \param role item
+     * \return icon
+     */
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole);
 
+    /*!
+     * \brief hash role->icon
+     */
     QHash<int,QString> hash;
 
-    void addToHash(Roles role, QString &path);
+    /*!
+     * \brief add pair role->icon to hash
+     * \param role for file
+     * \param path to icon
+     */
+    void setIconToRole(Roles role, QString &path);
 
 };
 
