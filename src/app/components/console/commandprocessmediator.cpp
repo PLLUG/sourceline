66d833b9d030b94c0c6b858a376ad2676ae69736
Olexandr Lynda <sanya.l9519@gmail.com>
2015-08-31 15:04:50 +0300

    Fixed code

--- src/app/components/console/commandprocessmediator.cpp
+++ src/app/components/console/commandprocessmediator.cpp
@@ -1,5 +1,26 @@
-#include "commandprocessmediator.h"
+/*******************************************************************************
+***                                                                          ***
+***    SourceLine - Crossplatform VCS Client.                                ***
+***    Copyright (C) 2015  by                                                ***
+***            Olexandr Lynda (sanya.l9519@gmail.com)                        ***
+***    This file is part of SourceLine Project.                              ***
+***                                                                          ***
+***    SourceLine is free software: you can redistribute it and/or modify    ***
+***    it under the terms of the GNU General Public License as published by  ***
+***    the Free Software Foundation, either version 3 of the License, or     ***
+***    (at your option) any later version.                                   ***
+***                                                                          ***
+***    SourceLine is distributed in the hope that it will be useful,         ***
+***    but WITHOUT ANY WARRANTY; without even the implied warranty of        ***
+***    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ***
+***    GNU General Public License for more details.                          ***
+***                                                                          ***
+***    You should have received a copy of the GNU General Public License     ***
+***    along with this program.  If not, see <http://www.gnu.org/licenses/>. ***
+***                                                                          ***
+*******************************************************************************/
 
+#include "commandprocessmediator.h"
 #include <QProcess>
 #include <QVariant>
 
@@ -7,7 +28,7 @@ CommandProcessMediator::CommandProcessMediator(QObject *parent) : QObject(parent
 {
     mProcess = new CommandProcess(this);
     // !!hardcode!! , dont work on linux , set for "shell" property "sh"
-    mProcess->setProperty("shell","C:\\Windows\\System32\\cmd");
+    mProcess->setProperty("shell","C:/Windows/System32/cmd");
     mProcess->setProperty("shellParam",QStringList()<< "/k");
     mProcess->start();
 
@@ -25,6 +46,7 @@ void CommandProcessMediator::processConsoleInput(QString input)
 
 void CommandProcessMediator::registerCommand(QString commandId, QString inputPattern)
 {
-
+    Q_UNUSED(commandId)
+    Q_UNUSED(inputPattern)
 }
 
--- src/app/components/console/commandprocessmediator.h
+++ src/app/components/console/commandprocessmediator.h
@@ -1,3 +1,26 @@
+/*******************************************************************************
+***                                                                          ***
+***    SourceLine - Crossplatform VCS Client.                                ***
+***    Copyright (C) 2015  by                                                ***
+***            Olexandr Lynda (sanya.l9519@gmail.com)                        ***
+***    This file is part of SourceLine Project.                              ***
+***                                                                          ***
+***    SourceLine is free software: you can redistribute it and/or modify    ***
+***    it under the terms of the GNU General Public License as published by  ***
+***    the Free Software Foundation, either version 3 of the License, or     ***
+***    (at your option) any later version.                                   ***
+***                                                                          ***
+***    SourceLine is distributed in the hope that it will be useful,         ***
+***    but WITHOUT ANY WARRANTY; without even the implied warranty of        ***
+***    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ***
+***    GNU General Public License for more details.                          ***
+***                                                                          ***
+***    You should have received a copy of the GNU General Public License     ***
+***    along with this program.  If not, see <http://www.gnu.org/licenses/>. ***
+***                                                                          ***
+*******************************************************************************/
+
+
 #ifndef COMMANDPROCESSMEDIATOR_H
 #define COMMANDPROCESSMEDIATOR_H
 
